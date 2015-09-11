a92091acf786c82c786876076cdd135f13918218
livoras <livoras@163.com>
2015-09-11 23:40:37 +0800

    增加disqus评论系统

--- src/scripts/components/disqus.js
+++ src/scripts/components/disqus.js
@@ -1,10 +1,22 @@
 class Disqus extends React.Component {
   constructor(props) {
     super(props)
+    let disqus_shortname = 'jiketao';
+    (function() {
+        var dsq = document.createElement('script')
+        dsq.type = 'text/javascript'
+        dsq.async = true
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] ||
+         document.getElementsByTagName('body')[0]).appendChild(dsq)
+    })();
   }
   render() {
     return (
-      <div>Disqus...</div>
+      <div className="disqus">
+      <div id="disqus_thread"></div>
+        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+      </div>
     )
   }
 }
