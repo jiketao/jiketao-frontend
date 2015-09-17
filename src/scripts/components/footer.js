eee0c4f0e66bd3a4aa13acfb858f4b695d7bf3a6
livoras <livoras@163.com>
2015-09-17 23:45:46 +0800

    调整Footer样式

--- src/scripts/components/footer.js
+++ src/scripts/components/footer.js
@@ -5,6 +5,12 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="footer">
+        <div className="footer-align">
+        <a href="#" className="cell">关于极客淘</a>
+        <a href="#" className="cell">Github</a>
+        <a href="#" className="cell">Team：特🐶，6174，livoras</a>
+        <a href="#" className="cell">Copyright@极客淘</a>
+        </div>
       </div>
     )
   }
